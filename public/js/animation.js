af1660d18c11df42f406c3f49c94a7b75fd47bef
tamura satoshi <stamura0926@gmail.com>
2025-07-03 11:26:07 +0900

    ローディング文字を出さない

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,39 +1,23 @@
-window.onload = () => {
-  const loading = document.getElementById("loading-screen");
+// MVアニメーション
+gsap.from(".mv-title", {
+    y: 40,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out"
+});
 
-  // まずローディング画面をフェードアウト
-  setTimeout(() => {
-    loading.style.opacity = "0";
-    loading.style.pointerEvents = "none";
+gsap.from(".mv-subtitle", {
+    y: 40,
+    opacity: 0,
+    duration: 1,
+    delay: 0.3,
+    ease: "power2.out"
+});
 
-    // ここで0.2秒後にGSAP開始（フェードアウト完了後）
-    setTimeout(() => {
-        loading.remove();
-
-        // MVアニメーション
-        gsap.from(".mv-title", {
-            y: 40,
-            opacity: 0,
-            duration: 1,
-            ease: "power2.out"
-        });
-
-        gsap.from(".mv-subtitle", {
-            y: 40,
-            opacity: 0,
-            duration: 1,
-            delay: 0.3,
-            ease: "power2.out"
-        });
-
-        gsap.from(".mv-text", {
-            y: 40,
-            opacity: 0,
-            duration: 1,
-            delay: 0.6,
-            ease: "power2.out"
-        });
-
-        }, 200); // ← フェードアウトが完了してからGSAP開始
-    }, 500); // ローディング表示時間
-};
+gsap.from(".mv-text", {
+    y: 40,
+    opacity: 0,
+    duration: 1,
+    delay: 0.6,
+    ease: "power2.out"
+});
